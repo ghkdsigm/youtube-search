624ebb97483fac2a03d20c9c3f73dcd30771a2c1
ghkdsigm3 <ghkdsigm3@gmail.com>
2022-08-01 02:23:20 +0900

    first commit

---
+++ youtube-search-master/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-12">
+          <h1 class="text-center">Youtube Search</h1>
+          <SearchBar />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-8">
+          <VideoPlayer />
+        </div>
+        <div class="col-4">
+          <VideoList />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SearchBar from "./components/SearchBar.vue";
+import VideoPlayer from "./components/VideoPlayer.vue";
+import VideoList from "./components/VideoList.vue";
+
+export default {
+  name: "App",
+  components: {
+    SearchBar,
+    VideoPlayer,
+    VideoList
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ youtube-search-master/src/components/VideoList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="video in videos" :key="video.id.videoId" @click="setVideo(video)">
+      <VideoItem :video="video" />
+    </div>
+  </div>
+</template>
+
+<script>
+import VideoItem from "./VideoItem";
+import { mapGetters, mapMutations } from "vuex";
+
+export default {
+  name: "VideoList",
+  components: {
+    VideoItem
+  },
+  computed: {
+    ...mapGetters(["videos"])
+  },
+  methods: {
+    ...mapMutations(["setVideo"])
+  }
+};
+</script>
+
+<style>
+</style>
